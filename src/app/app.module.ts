5d9473107fd82c8bd198615503a1902105f7f9da
huyen vu <huyen.vu@nextgencode.io>
2019-03-19 20:10:21 -0500

    add router to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+    ),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
