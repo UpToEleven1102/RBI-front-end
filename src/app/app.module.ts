5e321053c73a178e7ceecd64da929f03e3d97366
cmelchio <christian.melchiorre@ttu.edu>
2019-04-01 07:43:51 -0500

    message log

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { TopTenTableComponent } from './top-ten-table/top-ten-table.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopTenTableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
