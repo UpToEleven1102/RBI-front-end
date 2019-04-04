4beb078162a21de5d6c63b7849c8d6a007de7b09
huyen vu <huyen.vu@nextgencode.io>
2019-04-03 22:34:42 -0500

    done team page

--- src/app/Pages/team-page/team-page.component.ts
+++ src/app/Pages/team-page/team-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MainControllerService } from '../../Services/main-controller.service';
+import { Subscription } from 'rxjs';
+import gql from 'graphql-tag';
+import { Apollo } from 'apollo-angular';
 
 @Component({
   selector: 'app-team-page',
@@ -6,10 +11,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team-page.component.scss']
 })
 export class TeamPageComponent implements OnInit {
+  id: null;
+  team: null;
+  players: [];
+  subscription: Subscription;
+  displayedColumns: string[] = ['name', 'rbi', 'image', 'class', 'dob', 'home_town', 'ht_wt', 'action'];
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private mainCtrl: MainControllerService, private apollo: Apollo) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(params => {
+      this.id = params.id;
+      this.getTeam();
+    });
+  }
+
+  getTeamPlayers(team_id) {
+    this.subscription = this.apollo.watchQuery<any>({
+      query: gql`
+        {
+          teamPlayers(id: ${team_id}) {
+            id
+            name
+            player_img
+            class
+            ht_wt
+            home_town
+            dob
+            rbi
+          }
+        }
+      `
+    }).valueChanges.subscribe(result => {
+      this.players = result.data.teamPlayers;
+      console.log(this.players);
+      this.subscription.unsubscribe();
+    });
+  }
+
+  getTeam() {
+    this.subscription = this.apollo.watchQuery<any>({
+      query: gql`
+        {
+          team(id: ${this.id}){
+            id
+            name
+            university_name
+            team_img
+            conference {
+              id
+              name
+              nick_name
+              founded
+              member_number
+          }
+        }
+        }
+      `
+    }).valueChanges.subscribe(result => {
+      this.team = result.data.team;
+      console.log(this.team);
+      this.subscription.unsubscribe();
+      if (this.team) {
+        this.getTeamPlayers(result.data.team.id);
+      }
+    });
   }
 
 }
